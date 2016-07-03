91a5c6e1e16b6d2fc3b803b88003c3bd1201a137
bradshawit <nathan@bradshawit.com.au>
2016-07-03 13:31:28 +1000

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,7 +4,7 @@ set -e
 
 forever start /var/lib/xo-server
 
-npm start /var/lib/xo-server
+cd  /var/lib/xo-server & npm start
 
 touch /var/lib/xo-server/redis.log
 tail -f /var/lib/xo-server/redis.log &
