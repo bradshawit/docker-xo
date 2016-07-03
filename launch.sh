7b065678f853a6648c9f5cb1b311e2d799409879
bradshawit <nathan@bradshawit.com.au>
2016-07-03 11:08:49 +1000

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -6,5 +6,7 @@ echo never > /sys/kernel/mm/transparent_hugepage/enabled
 
 /etc/init.d/redis_6379 start
 
-forever start /var/lib/xo-server &
+forever start /var/lib/xo-server
+
+tail -f /var/lib/xo-server/redis.log &
 wait
